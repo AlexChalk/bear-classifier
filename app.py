77bff11b4b67a1b1b30d474b6696b52ce5224f4d
Alex Chalk <contact@alexchalk.net>
2024-05-29 23:59:15 -0400

    Use wider Image UI element

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def classify_image(image):
     return dict(zip(categories, map(float, probabilities)))
 
 
-image = gr.Image(height=192, width=192)
+image = gr.Image(height=256, width=256)
 label = gr.Label()
 examples = ["black.png", "grizzly.png", "teddy.png"]
 interface = gr.Interface(
