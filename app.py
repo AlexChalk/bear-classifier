cfcc02e36d4578599a6de9bbc944b0378b2c45db
Alex Chalk <contact@alexchalk.net>
2024-05-29 23:33:47 -0400

    Add bear classifier

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 import gradio as gr
+from fastai.vision.all import *
 
-def greet(name):
-    return "Hello " + name + "!!"
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-demo.launch()
+def classify_image(image):
+    categories = ("Black", "Grizzly", "Teddy")
+    learn = load_learner("export.pkl")
+    prediction, index, probabilities = learn.predict(image)
+    return dict(zip(categories, map(float, probabilities)))
+
+
+image = gr.inputs.Image(shape=(192, 192))
+label = gr.outputs.Label()
+examples = ["black.png", "grizzly.png", "teddy.png"]
+interface = gr.Interface(
+    fn=classify_image, inputs=image, outputs=label, examples=examples
+)
+interface.launch()
